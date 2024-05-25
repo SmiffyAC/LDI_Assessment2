de8e8aabd4d3d76ea47b99887b02c30732ce3efb
SmiffyAC <Aaron@smiffy.net>
2024-05-25 01:15:56 +0100

    Added individual files for stages and added commments to shell code

--- Clean_version/shell_Final.py
+++ Clean_version/shell_Final.py
@@ -1,13 +1,18 @@
 import Basic_final
 
 while True:
-	text = input('\nshell > ')
+	# Ask the user for input
+	text = input('\n>>> ')
+	# If the user entered nothing, continue
 	if text.strip() == "": continue
+	# Use <stdin> as a placeholder for the filename - calls the run function from Basic_final.py
 	result, error = Basic_final.run('<stdin>', text)
 
 	if error:
+		# If there is an error, print it
 		print(error.as_string())
 	elif result:
+		# If there is a result, print it
 		if len(result.elements) == 1:
 			print(repr(result.elements[0]))
 		else:
